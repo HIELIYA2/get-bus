81e04917bef391913419ffab3138e989811d6c92
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-05-03 21:52:38 +0300

    add board cmp

---
+++ client/src/components/Board.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="board">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Board',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
