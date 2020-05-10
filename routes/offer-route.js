d72dd87a5b29f7f756520d894e48b563eb3ac032
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-05-10 22:23:22 +0300

    create offer route backend

---
+++ routes/offer-route.js
@@ -0,0 +1,39 @@
+const offerService = require('../services/offerService');
+const OFFER_URL = '/api/offer';
+function addOffersRoutes(app) {
+  // offers REST API:
+
+  //OFFERS
+  app.get(OFFER_URL, (req, res) => {
+    offerService.query().then((offers) => res.json(offers));
+  });
+
+  // SINGLE - GET Full detail
+  app.get(`${OFFER_URL}/:offerId`, (req, res) => {
+    const offerId = req.params.offerId;
+    offerService.getOfferById(offerId).then((offer) => res.json(offer));
+  });
+
+  // DELETE
+  app.delete(`${OFFER_URL}/:offerId`, (req, res) => {
+    const offerId = req.params.offerId;
+    offerService
+      .removeOffer(offerId)
+      .then(() => res.end(`The Offer ${offerId} Was Deleted `));
+  });
+
+  // CREATE
+  app.post(OFFER_URL, (req, res) => {
+    const offer = req.body;
+    offerService.addOffer(offer).then((offer) => res.json(offer));
+  });
+
+  // UPDATE
+  app.put(`${OFFER_URL}/:offerId`, (req, res) => {
+    const offer = req.body;
+    delete offer.users;
+    offerService.updateOffer(offer).then((offer) => res.json(offer));
+  });
+}
+
+module.exports = addOffersRoutes;
