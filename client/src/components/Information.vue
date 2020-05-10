6863c86887d4f745d48a7fbf538f262152661a15
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-05-10 22:20:45 +0300

    create information cmp

---
+++ client/src/components/Information.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="information">
+    <header class="header"></header>
+
+    <main class="main items">
+      <div class="details">
+        <h1>{{ msg }}</h1>
+      </div>
+    </main>
+
+    <footer class="footer"></footer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Details',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      list: ['null', 'pop', 'lol', 'kok', 'hoh', 'mom', 'bob'],
+    };
+  },
+  components: {},
+  methods: {
+    newOrder() {
+      console.log(' new order');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.information {
+  max-height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  .header {
+  }
+  .main {
+    .details {
+      height: 260px;
+      width: 280px;
+      background-color: #fff;
+    }
+  }
+  .footer {
+  }
+}
+</style>
