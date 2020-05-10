ae93dc54b1bb23e1def435acfbdd5b4282812c5d
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-05-10 22:18:55 +0300

    edit board cmp

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -1,16 +1,65 @@
 <template>
   <div class="board">
-    <h1>{{ msg }}</h1>
+    <header class="header">
+      <h1>{{ msg }}</h1>
+    </header>
+
+    <main class="main">
+      <mapq msg="map"></mapq>
+      <information msg="information"></information>
+    </main>
+
+    <footer class="footer">
+      <bottom msg="bottom"></bottom>
+    </footer>
   </div>
 </template>
 
 <script>
+import information from './Information.vue';
+import mapq from './Map.vue';
+import bottom from './Bottom.vue';
+
 export default {
   name: 'Board',
   props: {
     msg: String,
   },
+  components: {
+    information,
+    mapq,
+    bottom,
+  },
 };
 </script>
 
-<style scoped></style>
+
+<style lang="scss" scoped>
+.board {
+  align-items: center;
+  flex-direction: column;
+
+  .header {
+    margin: 15px 0;
+  }
+  .main {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    .map {
+    }
+    .information {
+    }
+  }
+  .footer {
+    margin: 15px 0;
+    display: flex;
+    justify-content: center;
+    .bottom {
+      width: 600px;
+      height: 140px;
+      background-color: #fff;
+    }
+  }
+}
+</style>
