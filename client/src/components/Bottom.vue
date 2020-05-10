e7c2fe51708f19691acc88fab973de524c81e19b
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-05-10 22:19:22 +0300

    create bottom cmp

---
+++ client/src/components/Bottom.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="bottom">
+    <header class="header"></header>
+
+    <main class="main">
+      <div class="details">
+        <h1>{{ msg }}</h1>
+      </div>
+    </main>
+
+    <footer class="footer"></footer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Bottom',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {
+    newOrder() {
+      console.log(' new order');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.botton {
+  max-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  .header {
+  }
+  .main {
+    .details {
+      height: 260px;
+      width: 260px;
+      background-color: #fff;
+    }
+  }
+  .footer {
+  }
+}
+</style>
