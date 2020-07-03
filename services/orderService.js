5280b8e1d3bcfd7adff5c8a42d638e7cb716af66
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-07-03 08:49:48 +0300

    Update order service

--- services/orderService.js
+++ services/orderService.js
@@ -1,13 +1,50 @@
 const mongoService = require('./mongoService');
+const userService = require('./userService');
 const ORDERS_DB = 'orders';
+const USERS_DB = 'users';
 
 const ObjectId = require('mongodb').ObjectId;
 
-function query() {}
+function query() {
+  return mongoService
+    .connect()
+    .then((db) => db.collection(ORDERS_DB).find({}).toArray());
+}
 
-function addOrder(order) {}
+function addOrder(order) {
+  console.log('addOrder', order);
+  return mongoService.connect().then((db) => {
+    const collection = db.collection(ORDERS_DB);
+    return collection.insertOne(order).then((result) => {
+      order._id = result.insertedId;
+      userService.addOrderID(order, order._id);
+      return order;
+    });
+  });
+}
 
-function getOrderById(orderId) {}
+function addOfferID(offer, offerId) {
+  console.log('update add offer ID', offerId, offer);
+  return mongoService.connect().then((db) => {
+    const orderID = new ObjectId(offer.orderID);
+    console.log('orderID:', orderID);
+    const collection = db.collection(ORDERS_DB);
+    return collection.updateOne(
+      { _id: orderID },
+      { $push: { offers: offer._id } }
+    );
+  });
+}
+
+function getOrderById(orderId) {
+  orderId = new ObjectId(orderId);
+  return mongoService.connect().then(async (db) => {
+    const collection = db.collection(ORDERS_DB);
+    const x = await collection.findOne({ _id: orderId });
+    console.log(x);
+    return x;
+  });
+}
 
 function removeOrder(orderId) {
   const _id = new ObjectId(orderId);
@@ -34,6 +71,7 @@ function updateOrder(order) {
 module.exports = {
   query,
   addOrder,
+  addOfferID,
   getOrderById,
   removeOrder,
   updateOrder,
