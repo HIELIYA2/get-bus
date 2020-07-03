edc7096af4448b822de1ab65cce3f7467ce79329
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-07-03 11:13:36 +0300

    line-break style in aslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -7,9 +7,14 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
+  quotes: [2, 'single'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': 0,
+  },
+  globals: {
+    SwaggerEditor: false,
   },
   overrides: [
     {
