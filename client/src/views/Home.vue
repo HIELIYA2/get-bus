604df46e79d2c12d86ef9652439efa2576a0ac2d
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-07-03 08:44:38 +0300

    Update home cmp

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,11 +6,21 @@
 </template>
 
 <script>
+/*
+eslint no-underscore-dangle: ["error", { "allow": ["_id"] }]
+*/
 import HomeHeader from '@/components/HomeHeader.vue';
 import HomeFotter from '@/components/HomeFotter.vue';
 
 export default {
   name: 'Home',
+  created() {
+    if (this.$store.getters.isUserLoggedIn) {
+      const user = this.$store.getters.loggedInUser;
+      console.log(user);
+      this.$store.dispatch({ type: 'updateUser', userId: user._id });
+    }
+  },
   components: {
     HomeHeader,
     HomeFotter,
