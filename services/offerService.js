12fb945b830f317b25d6c3aa6568b88ad7c6f12d
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-07-03 08:50:22 +0300

    Update offer service

--- services/offerService.js
+++ services/offerService.js
@@ -1,14 +1,38 @@
 const mongoService = require('./mongoService');
-const OFFERS_DB = 'orders';
+const orderService = require('./orderService');
+const userService = require('./userService');
+const OFFERS_DB = 'offers';
 
 const ObjectId = require('mongodb').ObjectId;
 
-function query() {}
+function query() {
+  return mongoService
+    .connect()
+    .then((db) => db.collection(OFFERS_DB).find({}).toArray());
+}
 
-function addOffer(offer) {}
+function addOffer(offer) {
+  console.log('add offer', offer);
+  return mongoService.connect().then((db) => {
+    const collection = db.collection(OFFERS_DB);
+    return collection.insertOne(offer).then((result) => {
+      offer._id = result.insertedId;
+      orderService.addOfferID(offer, offer._id);
+      userService.addOfferID(offer, offer._id);
+      return offer;
+    });
+  });
+}
 
-function getOfferById(offerId) {}
-
+function getOfferById(offerId) {
+  offerId = new ObjectId(offerId);
+  return mongoService.connect().then(async (db) => {
+    const collection = db.collection(OFFERS_DB);
+    const x = await collection.findOne({ _id: offerId });
+    console.log(x);
+    return x;
+  });
+}
 function removeOffer(offerId) {
   const _id = new ObjectId(offerId);
   return mongoService
