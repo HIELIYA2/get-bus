c8f4882e085f5d58f7dca3fa46eea4ee31830f35
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-07-03 08:44:04 +0300

    Update index store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import orderStore from '../../modules/orderStore';
+import offerStore from '../../modules/offerStore';
+import userStore from '../../modules/userStore';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: { userStore, orderStore, offerStore },
 });
