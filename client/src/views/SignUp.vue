e799d1d4eb03644c86ec79e1bf496608460cce0e
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-08-02 01:39:41 +0300

    edit pic login cmp

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sign-up">
-    <img src="../assets/imgs/6.png" alt class="sign-up-img" />
+    <img src="../assets/imgs/7.jpg" alt class="sign-up-img" />
 
     <form class="form-sign-up" @submit.prevent="submit">
       <div class="up-in-toggle">
@@ -107,7 +107,7 @@ export default {
 
 <style lang="scss" scoped>
 .sign-up {
-  margin-top: calc(50vh - 170px);
+  // margin-top: calc(50vh - 170px);
   .sign-up-img {
     top: 0;
     left: 0;
@@ -116,7 +116,9 @@ export default {
     width: 100vw;
     height: 100vh;
     object-fit: cover;
-    filter: brightness(65%);
+    filter: brightness(85%);
+    filter: contrast(80%);
+    filter: grayscale(50%);
   }
   .sign-up-img {
     top: 0;
@@ -128,10 +130,61 @@ export default {
     object-fit: cover;
   }
   .form-sign-up {
+    margin-top: 95px;
+    width: fit-content;
+    height: calc(100vh - 115px);
+    margin-left: 15vw;
+    padding: 0 20px 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    background-color: rgba(209, 207, 207, 0.356);
+    border-radius: 4%;
+    .up-in-toggle {
+      padding: 10px 0;
+      input {
+        clip: rect(0, 0, 0, 0);
+        height: 0.1px;
+        width: 0.1px;
+        border: 0;
+        overflow: hidden;
+        &:checked + label {
+          background: rgb(139, 200, 254);
+          background: linear-gradient(
+            0deg,
+            rgb(139, 200, 254) 0%,
+            rgba(139, 200, 254, 1) 100%
+          );
+          color: white;
+          font-weight: 500;
+          box-shadow: 0 7px 20px rgba(0, 0, 0, 0.09),
+            0 6px 6px rgba(0, 0, 0, 0.13);
+        }
+      }
+      label {
+        display: inline-block;
+        width: 100px;
+        background-color: rgba(42, 42, 42, 0.473);
+        color: rgb(165, 173, 183);
+        text-align: center;
+        font-size: 0.8rem;
+        padding: 6px 6px;
+        float: left;
+        transition: all 0.1s ease-in-out;
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+  }
+
+  .up-in-toggle label:first-of-type {
+    border-radius: 15.5px 0 0 15.5px;
+  }
+
+  .up-in-toggle label:last-of-type {
+    border-radius: 0 15.5px 15.5px 0;
   }
   .first-name-sign-up,
   .last-name-sign-up,
@@ -146,9 +199,9 @@ export default {
     height: 50px;
     border: 0px solid rgb(31, 31, 31);
     color: rgb(255, 255, 255);
-    background-color: rgba(136, 136, 136, 0.274);
+    background-color: rgba(42, 42, 42, 0.473);
     margin: 7px 0;
-    border-radius: 50px;
+    border-radius: 10px;
   }
   .first-name-sign-up::placeholder,
   .last-name-sign-up::placeholder,
@@ -169,50 +222,5 @@ export default {
   .wrong {
     border: 2px solid red;
   }
-}
-
-.up-in-toggle {
-  padding: 7px 0;
-  input {
-    clip: rect(0, 0, 0, 0);
-    height: 1px;
-    width: 1px;
-    border: 0;
-    overflow: hidden;
-    &:checked + label {
-      background: rgb(139, 200, 254);
-      background: linear-gradient(
-        0deg,
-        rgb(139, 200, 254) 0%,
-        rgba(139, 200, 254, 1) 100%
-      );
-      color: white;
-      font-weight: 500;
-      box-shadow: 0 7px 20px rgba(0, 0, 0, 0.09), 0 6px 6px rgba(0, 0, 0, 0.13);
-    }
-  }
-  label {
-    display: inline-block;
-    width: 100px;
-    background-color: rgb(73, 90, 111);
-    color: rgb(165, 173, 183);
-    text-align: center;
-    font-family: 'Quicksand', sans-serif;
-    font-size: 0.8rem;
-    padding: 6px 6px;
-    float: left;
-    transition: all 0.1s ease-in-out;
-    &:hover {
-      cursor: pointer;
-    }
-  }
-}
-
-.up-in-toggle label:first-of-type {
-  border-radius: 15.5px 0 0 15.5px;
-}
-
-.up-in-toggle label:last-of-type {
-  border-radius: 0 15.5px 15.5px 0;
 }
 </style>
