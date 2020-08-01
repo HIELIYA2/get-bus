ab5b29a44bb2819240f82162836a2f6d46293fbb
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-08-02 01:32:44 +0300

    edit .eslimt

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -7,14 +7,10 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
-  quotes: [2, 'single'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 0,
-  },
-  globals: {
-    SwaggerEditor: false,
   },
   overrides: [
     {
