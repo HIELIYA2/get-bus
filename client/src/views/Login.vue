4fb2595abe81ff1f8b3c94e554aa7beaecf524eb
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-08-02 01:39:03 +0300

    edit pic modal  cmp

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="login">
-    <img src="../assets/imgs/6.png" alt class="login-img" />
+    <img src="../assets/imgs/7.jpg" alt class="login-img" />
 
-    <div>
+    <div class="login-options">
       <form class="form-login" @submit.prevent="login">
         <input
           class="username-login"
@@ -22,11 +22,11 @@
         />
         <button class="btn-login" type="submit">Log in</button>
       </form>
-    </div>
-    <div class="login-creat">
-      <button class="btn-new-acount">
-        <router-link to="/signup/">Create acount</router-link>
-      </button>
+      <div class="login-creat">
+        <button class="btn-new-acount">
+          <router-link to="/signup/">Create acount</router-link>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -51,6 +51,9 @@ export default {
           user: this.user,
         })
         .then((res) => {
+          if (res === null) {
+            this.isWrong = true;
+          }
           const { userName } = JSON.parse(JSON.stringify(res));
           if (res.type === 'Driver') {
             console.log('dispach', res);
@@ -59,8 +62,6 @@ export default {
           if (userName.length) {
             this.isWrong = false;
             this.$router.push({ path: '/' });
-          } else {
-            this.isWrong = true;
           }
         });
     },
@@ -71,8 +72,6 @@ export default {
 
 <style lang="scss" scoped>
 .login {
-  margin-top: 75px;
-  padding: 120px 0;
   .login-img {
     top: 0;
     left: 0;
@@ -81,59 +80,69 @@ export default {
     width: 100vw;
     height: 100vh;
     object-fit: cover;
-    filter: brightness(65%);
+    filter: brightness(50%);
   }
-  .form-login {
+  .login-options {
+    margin-top: 95px;
+    width: fit-content;
+    height: calc(100vh - 115px);
+    margin-left: 15vw;
+    padding: 0 20px 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
-  }
-  .username-login,
-  .password-login,
-  .btn-login,
-  .btn-new-acount {
-    font-size: 18px;
-    font-family: PontanoSans-Regular;
-    padding: 15px;
-    width: 350px;
-    height: 50px;
-    border: 0px solid rgb(31, 31, 31);
-    color: rgb(255, 255, 255);
-    background-color: rgba(61, 61, 61, 0.274);
-    margin: 7px 0;
-    border-radius: 50px;
-  }
-  .username-login::placeholder,
-  .password-login::placeholder {
-    color: rgba(255, 255, 255, 0.808);
-  }
-  a {
-    color: rgb(255, 255, 255);
-    transition: 0.3s;
-    padding: 13px 95px;
-    border-radius: 50px;
-    &:hover {
+    align-items: flex-end;
+    background-color: rgba(187, 187, 187, 0.315);
+    .form-login {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    .username-login,
+    .password-login,
+    .btn-login,
+    .btn-new-acount {
+      font-size: 18px;
+      font-family: PontanoSans-Regular;
+      padding: 15px;
+      width: 350px;
+      height: 50px;
+      border: 0px solid rgb(31, 31, 31);
+      color: rgb(255, 255, 255);
+      background-color: rgba(61, 61, 61, 0.274);
+      margin: 7px 0;
+      border-radius: 50px;
+    }
+    .username-login::placeholder,
+    .password-login::placeholder {
+      color: rgba(255, 255, 255, 0.808);
+    }
+    a {
+      color: rgb(255, 255, 255);
+      transition: 0.3s;
+      padding: 13px 95px;
+      border-radius: 50px;
+      &:hover {
+        color: rgb(255, 255, 255);
+      }
+    }
+    .btn-login,
+    .btn-new-acount {
+      cursor: pointer;
+      transition: 0.3s;
+      background-color: #8dc9fe;
+    }
+    .btn-login:hover,
+    .btn-new-acount:hover {
+      background-color: #63b3fa;
       color: rgb(255, 255, 255);
     }
-  }
-  .btn-login,
-  .btn-new-acount {
-    cursor: pointer;
-    transition: 0.3s;
-    background-color: #8dc9fe;
-  }
-  .btn-login:hover,
-  .btn-new-acount:hover {
-    background-color: #63b3fa;
-    color: rgb(255, 255, 255);
-  }
-  .wrong {
-    border: 2px solid red;
-  }
-  .login-creat {
-    display: flex;
-    justify-content: center;
+    .wrong {
+      border: 2px solid red;
+    }
+    .login-creat {
+      display: flex;
+    }
   }
 }
 </style>
