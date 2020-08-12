f10e32f6ba3e4060e749725511bc0ac0769a1474
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-08-12 09:23:07 +0300

    about cmp new details

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section class="section-1">
+      <div class="about-caption">
+        <h1 class="about-h1">GETBUS</h1>
+        <h2 class="about-h2">Everywhere</h2>
+        <h3 class="about-p1">
+          With a national network,
+        </h3>
+        <h3 class="about-p2">
+          Next step in the world of transportation
+        </h3>
+      </div>
+      <img src="../assets/imgs/12.png" alt="" class="about-phone" />
+    </section>
+    <section class="section-2">
+      <img src="../assets/imgs/16.gif" alt="" class="about-img about-land" />
+      <div class="about-caption">
+        <h1 class="about-h1">GETBUS</h1>
+        <h2 class="about-h2">Everywhere</h2>
+        <h3 class="about-p1">
+          With a national network,
+        </h3>
+        <h3 class="about-p2">
+          Next step in the world of transportation
+        </h3>
+      </div>
+    </section>
+    <section class="section-3"></section>
   </div>
 </template>
 
@@ -16,5 +42,55 @@ export default {
   padding-top: 75px;
   min-height: 100vh;
   background-color: rgb(220, 241, 245);
+  .section-1 {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    height: 100vh;
+    .about-phone {
+      height: 80vh;
+    }
+  }
+  .section-2 {
+    z-index: -10;
+    width: calc(100vw - 3px);
+    height: 100vh;
+    object-fit: cover;
+    background-image: url('../assets/imgs/15.jpg');
+    background-size: cover;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    .about-land {
+      align-items: center;
+      justify-content: flex-start;
+      z-index: 3;
+      height: 80vh;
+    }
+    .about-caption {
+      font-family: OpenSansCondensed-Light;
+      padding-bottom: 150px;
+      .about-h1 {
+        font-size: 50px;
+        padding-bottom: 5px;
+        font-weight: bold;
+      }
+      .about-h2 {
+        font-size: 40px;
+        padding-bottom: 20px;
+      }
+      .about-p1,
+      .about-p2 {
+        font-size: 30px;
+        padding-bottom: 5px;
+      }
+    }
+  }
+  .section-3 {
+    height: 100vh;
+    background-color: darkcyan;
+  }
 }
 </style>
