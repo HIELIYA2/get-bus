69cb84c80054ada64d703926cb64b35709338b70
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-08-13 05:32:16 +0300

    fix direct after build

---
+++ public/apikey.js
@@ -0,0 +1,2 @@
+// src =
+//   'https://maps.googleapis.com/maps/api/js?key=AIzaSyD8oA9Pm_l4ZywfUkVUx_D1Ew8NJcF6N_4&libraries=places';
---
+++ public/precache-manifest.038819a29277614b554549ebe528ba8b.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fc024df3a7ad442140a51366b37454",
+    "url": "/apikey.js"
+  },
+  {
+    "revision": "0bedb58232e2acb388a4",
+    "url": "/css/app.a41a4087.css"
+  },
+  {
+    "revision": "599c84c2d21732a5b0e5",
+    "url": "/css/chunk-vendors.ac0834e4.css"
+  },
+  {
+    "revision": "cf2842ec05dc062e6a7c67ff09e83fd8",
+    "url": "/fonts/Acme-Regular.cf2842ec.ttf"
+  },
+  {
+    "revision": "66e2160a3481cc78b7684014fadcc1c3",
+    "url": "/fonts/FingerPaint-Regular.66e2160a.ttf"
+  },
+  {
+    "revision": "eb9532033c2adf99b1314611b5e9cd0e",
+    "url": "/fonts/Lato_Bold.eb953203.ttf"
+  },
+  {
+    "revision": "3b9b99039cc0a98dd50c3cbfac57ccb2",
+    "url": "/fonts/Lato_Regular.3b9b9903.ttf"
+  },
+  {
+    "revision": "3cbe9257e888255cb2dad0d8a80a7759",
+    "url": "/fonts/NanumGothic-Regular.3cbe9257.ttf"
+  },
+  {
+    "revision": "3589bddbe338e444d408f4dbc545ca1e",
+    "url": "/fonts/OpenSansCondensed-Light.3589bddb.ttf"
+  },
+  {
+    "revision": "54f7b1a951e2c6c3884bdc803ae03c25",
+    "url": "/fonts/PontanoSans-Regular.54f7b1a9.ttf"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/fonts/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "8ce15b1564d76cd81b4af7ecf1e03125",
+    "url": "/google.js"
+  },
+  {
+    "revision": "59254f8dcad72fc18072bad2623380d8",
+    "url": "/img/11.59254f8d.jpg"
+  },
+  {
+    "revision": "f5dd1bd6c3195a43339c3a4f7db458cd",
+    "url": "/img/12.f5dd1bd6.png"
+  },
+  {
+    "revision": "170a1002b8204b9b5540f737b0ccc371",
+    "url": "/img/15.170a1002.jpg"
+  },
+  {
+    "revision": "552a1ededcf2a3a500c30b23128ba3f5",
+    "url": "/img/6.552a1ede.jpg"
+  },
+  {
+    "revision": "dd793ae9d9e71e319afc7e02ff3a7278",
+    "url": "/img/6.dd793ae9.png"
+  },
+  {
+    "revision": "7e9dafbb1c186449428a30fe80b4e9f4",
+    "url": "/img/7.7e9dafbb.jpg"
+  },
+  {
+    "revision": "8580fb688f639a845a068d9d9cd093ab",
+    "url": "/img/arrow-down.8580fb68.svg"
+  },
+  {
+    "revision": "f04c1b8c32ec83254b28d8f3b371ab8a",
+    "url": "/img/clock.f04c1b8c.svg"
+  },
+  {
+    "revision": "a59eee62aa935454a97dda4563c9dbd1",
+    "url": "/img/map.a59eee62.svg"
+  },
+  {
+    "revision": "17e63c7626d06498e9a3de4b23453eb6",
+    "url": "/img/paper.17e63c76.svg"
+  },
+  {
+    "revision": "b46aa48e9e613a9f5fb30a9deefb2dad",
+    "url": "/img/placeholder.b46aa48e.svg"
+  },
+  {
+    "revision": "5b8f855ebf2f878453c206ab57d69e95",
+    "url": "/img/route.5b8f855e.svg"
+  },
+  {
+    "revision": "82432ec88110b678e02688111d5dbfb8",
+    "url": "/img/search.82432ec8.svg"
+  },
+  {
+    "revision": "8bd39c1c66b24ca772345e3af40709c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bedb58232e2acb388a4",
+    "url": "/js/app.caf52193.js"
+  },
+  {
+    "revision": "599c84c2d21732a5b0e5",
+    "url": "/js/chunk-vendors.538d0586.js"
+  },
+  {
+    "revision": "6e38f82ab848cbe156f9c0854d6507b8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
